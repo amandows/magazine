6b24f5172ffcf89421a48307819be1b1feea4670
Amantur Toktosunov <amandows>
2023-02-14 15:00:54 +0600

    ed commit

--- src/js/vue.js
+++ src/js/vue.js
@@ -1,15 +1,34 @@
+let swiper1 = document.querySelector('.swiper-1')
+var swiperOne = new Swiper(swiper1, {
+    loop: true,
+    cssMode: true,
+    autoplay: {
+        delay: 5000,
+        disableOnInteraction: false,
+      },
+    navigation: {
+        nextEl: ".prev1",
+        prevEl: ".next1",
+    },
+    pagination: {
+        el: ".pg1",
+        dynamicBullets: true,
+    },
+    mousewheel: true,
+    keyboard: true,
+});
 
-let one = 0
-button.addEventListener("click", () => {
+let menuBtn = document.querySelector('.menu-btn')
+let menuBurger = document.querySelector('.burger-menu ')
+let one = 0 
+menuBtn.addEventListener("click", () => {
     one++
     if(one % 2 == 0) {
-        grid.style.cssText = "grid-template-areas: \"header header header\" \"main main main\" \"footer footer footer\";";
-        aside.style.cssText = "display: none;";
-        depart.style.cssText = "transform: rotate(0)"
+        menuBurger.style.cssText = "top: -100%;"
+        menuBtn.style.cssText = "transform: rotate(0)"
     }
     else if(one % 2 !== 0) {
-        grid.style.cssText = "grid-template-areas: \"header header header\" \"aside main main\" \"footer footer footer\";";
-        aside.style.cssText = "display: block;";
-        depart.style.cssText = "transform: rotate(90deg)"
+        menuBurger.style.cssText = "top: 50px;"
+        menuBtn.style.cssText = "transform: rotate(90deg)"
     }
 })
